22c2b163d12f044fc59ef444997734245b834289
Bill <96billc@gmail.com>
2023-03-18 10:06:45 +0000

    simple as

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import * as React from "react"
-import { useState, useEffect } from "react"
-import "./App.css"
+import * as React from "react";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 export default function App() {
-  const [article, setArticle] = useState(null)
+  const [article, setArticle] = useState(null);
 
   const fetchRandomArticle = async () => {
     const response = await fetch(
       "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*"
-    )
-    const data = await response.json()
-    const pageId = data.query.random[0].id
+    );
+    const data = await response.json();
+    const pageId = data.query.random[0].id;
 
     const articleResponse = await fetch(
       `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info|links&inprop=url&pageids=${pageId}&pllimit=500&origin=*`
-    )
-    const articleData = await articleResponse.json()
-    const fetchedArticle = articleData.query.pages[pageId]
+    );
+    const articleData = await articleResponse.json();
+    const fetchedArticle = articleData.query.pages[pageId];
 
     if (fetchedArticle.links && fetchedArticle.links.length >= 10) {
-      setArticle(fetchedArticle)
+      setArticle(fetchedArticle);
     } else {
-      fetchRandomArticle()
+      fetchRandomArticle();
     }
-  }
+  };
 
   useEffect(() => {
-    fetchRandomArticle()
-  }, [])
+    fetchRandomArticle();
+  }, []);
 
   return (
     <div className="App">
@@ -37,13 +37,13 @@ export default function App() {
       </header>
       {article && (
         <div className="Article">
-          {/* <h2>{article.title}</h2>
+          <h2>{article.title}</h2>
           <h3>Links: {article.links.length}</h3>
           <a href={article.fullurl} target="_blank" rel="noreferrer">
             Read on Wikipedia
-          </a> */}
+          </a>
         </div>
       )}
     </div>
-  )
+  );
 }
