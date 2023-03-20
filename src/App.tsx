dc7491a495380ea17ad1382f9d67e2ea6500af95
Bill <96billc@gmail.com>
2023-03-20 23:39:55 +0000

    minmax

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,27 @@ interface Article {
 
 export default function App() {
   const [article, setArticle] = useState<Article>();
+  const [minLinks, setMinLinks] = useState(10);
+  const [maxLinks, setMaxLinks] = useState(50);
 
   const fetchRandomArticle = async () => {
     const response = await fetch(
-      "https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*"
+      `https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=1&origin=*`
     );
     const data = await response.json();
     const pageId = data.query.random[0].id;
 
     const articleResponse = await fetch(
-      `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info|linkshere|extracts&inprop=url&exintro&explaintext&pageids=${pageId}&lhlimit=500&origin=*`
+      `https://en.wikipedia.org/w/api.php?action=query&format=json&prop=info|linkshere|extracts&inprop=url&exintro&explaintext&pageids=${pageId}&lhlimit=${maxLinks}&origin=*`
     );
     const articleData = await articleResponse.json();
     const fetchedArticle = articleData.query.pages[pageId] as Article;
 
-    if (fetchedArticle.linkshere && fetchedArticle.linkshere.length >= 10) {
+    if (
+      fetchedArticle.linkshere &&
+      fetchedArticle.linkshere.length >= minLinks &&
+      fetchedArticle.linkshere.length <= maxLinks
+    ) {
       setArticle(fetchedArticle);
     } else {
       fetchRandomArticle();
@@ -36,10 +42,42 @@ export default function App() {
     fetchRandomArticle();
   }, []);
 
+  const handleMinLinksChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMinLinks(Number(event.target.value));
+  };
+
+  const handleMaxLinksChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setMaxLinks(Number(event.target.value));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Random Wikipedia Article</h1>
+        <div>
+          <label htmlFor="minLinks">Minimum Links:</label>
+          <input
+            type="number"
+            id="minLinks"
+            name="minLinks"
+            min="10"
+            max="500"
+            value={minLinks}
+            onChange={handleMinLinksChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="maxLinks">Maximum Links:</label>
+          <input
+            type="number"
+            id="maxLinks"
+            name="maxLinks"
+            min="10"
+            max="500"
+            value={maxLinks}
+            onChange={handleMaxLinksChange}
+          />
+        </div>
         <button onClick={fetchRandomArticle}>Fetch New Article</button>
       </header>
       {article && (
